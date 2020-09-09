0edf0b9896ca93dddff7c55b7704eff9954a8489
Alvin Adhitama <51202525+alvinadhitama@users.noreply.github.com>
2020-09-09 23:37:15 +0700

    update key firebase

--- src/config/Firebase/index.js
+++ src/config/Firebase/index.js
@@ -1,13 +1,7 @@
 import firebase from 'firebase';
 
 firebase.initializeApp({
-  apiKey: 'AIzaSyBEMtQHioB2XY8u5mdXet1X8og1WoSk_nU',
-  authDomain: 'konsultasi-katolik.firebaseapp.com',
-  databaseURL: 'https://konsultasi-katolik.firebaseio.com',
-  projectId: 'konsultasi-katolik',
-  storageBucket: 'konsultasi-katolik.appspot.com',
-  messagingSenderId: '116358031144',
-  appId: '1:116358031144:web:d47ad506deb92fdafe121d',
+  //secret
 });
 
 const Firebase = firebase;
